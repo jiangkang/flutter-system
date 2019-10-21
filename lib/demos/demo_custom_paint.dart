b9dcb72e33e8387e617d9b62626d38d942d587d2
jiangkang <jiangkang0504@163.com>
2019-10-21 22:29:09 +0800

    优化围棋棋盘

--- lib/demos/demo_custom_paint.dart
+++ lib/demos/demo_custom_paint.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:ui' as prefix0;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -91,24 +92,41 @@ class SimplePainter extends CustomPainter {
     ..color = Colors.white
     ..style = PaintingStyle.fill;
 
+  final blacks = [
+    Offset(2, 1),
+    Offset(2, 2),
+    Offset(2, 0),
+    Offset(11, 2),
+    Offset(13, 7),
+    Offset(7, 7),
+  ];
+  final whites = [
+    Offset(2, 3),
+    Offset(3, 2),
+    Offset(1, 2),
+    Offset(8, 3),
+    Offset(9, 12),
+    Offset(10, 11),
+  ];
+
   @override
   void paint(Canvas canvas, Size size) {
     double cellWidth = size.width / 18;
     double cellHeight = size.height / 18;
 
-    canvas.drawCircle(Offset(cellWidth * 2, cellHeight * 2), 9, blackPaint);
-    canvas.drawCircle(Offset(cellWidth * 2, cellHeight * 1), 9, blackPaint);
-    canvas.drawCircle(Offset(cellWidth * 2, cellHeight * 0), 9, blackPaint);
-    canvas.drawCircle(Offset(cellWidth * 2, cellHeight * 3), 9, whitePaint);
-    canvas.drawCircle(Offset(cellWidth * 3, cellHeight * 2), 9, whitePaint);
-    canvas.drawCircle(Offset(cellWidth * 1, cellHeight * 2), 9, whitePaint);
+    blacks.forEach((offset) {
+      blackPaint
+        ..shader = prefix0.Gradient.radial(
+            offset * cellWidth, 9, [Colors.black, Colors.grey[850]]);
+      canvas.drawCircle(offset * cellWidth, 9, blackPaint);
+    });
 
-    canvas.drawCircle(Offset(cellWidth * 11, cellHeight * 2), 9, blackPaint);
-    canvas.drawCircle(Offset(cellWidth * 13, cellHeight * 7), 9, blackPaint);
-    canvas.drawCircle(Offset(cellWidth * 7, cellHeight * 7), 9, blackPaint);
-    canvas.drawCircle(Offset(cellWidth * 8, cellHeight * 3), 9, whitePaint);
-    canvas.drawCircle(Offset(cellWidth * 9, cellHeight * 12), 9, whitePaint);
-    canvas.drawCircle(Offset(cellWidth * 10, cellHeight * 11), 9, whitePaint);
+    whites.forEach((offset) {
+      whitePaint
+        ..shader = prefix0.Gradient.radial(
+            offset * cellWidth, 9, [Colors.white, Colors.grey[100]]);
+      canvas.drawCircle(offset * cellWidth, 9, whitePaint);
+    });
   }
 
   @override
