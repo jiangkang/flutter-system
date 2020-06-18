7a7539912d6cbdaccc66b612b79ac54e960d2e3e
jiangkang <jiangkang0504@163.com>
2020-06-18 20:51:13 +0800

    新增SplashScreen

--- android/app/src/main/kotlin/com/jiangkang/flutter_system/MainActivity.kt
+++ android/app/src/main/kotlin/com/jiangkang/flutter_system/MainActivity.kt
@@ -1,22 +1,7 @@
 package com.jiangkang.flutter_system
 
-import android.os.Bundle
-import android.widget.Toast
-import io.flutter.app.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-        MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result -> runOnUiThread { Toast.makeText(this@MainActivity, call.arguments.toString(), Toast.LENGTH_SHORT).show() } }
-    }
-
-
-    companion object {
-
-        private val CHANNEL = "com.jiangkang.flutter_system/toast"
-    }
+    
 }
